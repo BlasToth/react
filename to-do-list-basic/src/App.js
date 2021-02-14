70307caa118c598292e7f42a3786802a54edb73c
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-14 16:26:47 +0100

    add buutons

--- to-do-list-basic/src/App.js
+++ to-do-list-basic/src/App.js
@@ -3,8 +3,13 @@ import TodoList from './TodoList'
 
 function App() {
   return (
-    <TodoList />
-    <input type="text" />
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Todos</button>
+      <div>0 left to do</div>
+    </>
   )
 }
 
